9f18d85b507e0d812ff15c5f87ad3f34607906b7
Sisay2009 <139247647+Sisay2009@users.noreply.github.com>
2023-07-13 15:00:37 -0400

    Add files via upload

--- StreamConvertor.py
+++ StreamConvertor.py
@@ -8,10 +8,30 @@ Num_Bridges = 2
 TotalNumStreams = 0
 ListOfStreams = []
 ListOfIDs = []
+TotalDevice_List = []
 ListOfFramesPerPeriod = []
 ListOfMaxFrameSize = []
 ListOfRefreshPeriod = []
 ListOfIpAddress = []
+ListOfMacAddress = []
+
+####### Parsing RT Tologogy ########################
+with open("rt.xml", "r") as f:
+    data = f.read()
+tree = ET.parse('rt.xml')
+root = tree.getroot()
+
+for i in range (19):
+	TotalDevice_List.append(root[i][0].text)
+
+for elem in tree.iter():
+    if elem.tag==str("{urn:GE:std:802.1Q:yang:rich-topology}interfaceMacAddress"):
+        print(elem.tag, elem.text)
+        ListOfMacAddress.append(elem.text)
+    
+#for l in range (Num_End_Device-1):
+    #ListOfMacAddress.append("00-0a-35-00-01-"+str((l+2)*10))
+print (ListOfMacAddress)
 
 ################Stream file reading and parsing###########
 with open("Stream_Level_ICD_V1.xml", "r") as f:
@@ -25,7 +45,8 @@ for l in range (Num_End_Device*2):
     ListOfIpAddress.append("10.1.1."+str(l+2))
 print (ListOfIpAddress)
 
-	########################Total Number of Streams###################
+	
+    ########################Total Number of Streams###################
 for elem in stream.iter():
     if (elem.tag=="Stream"):
         TotalNumStreams=TotalNumStreams+1
@@ -64,74 +85,76 @@ print ("The total List of ListOfRefreshPeriod is=", ListOfRefreshPeriod)
 
 ############################## Generating QCC file ########################
 def GeneratQcc(Qcc):
-	Root_Qcc= ET.Element("qcc-inputs")
-	Root_Qcc.set("xmlns", "urn:ieee:std:802.1Q:yang:ge-qcc")
-	
-	for i in range (len(ListOfStreams)):
-		stream = ET.SubElement(Root_Qcc, "stream-configuration")
-		streamID=ET.SubElement(stream, "stream-id")
-		streamID.text=(ListOfStreams[i])#"00-0a-35-00-01-10:17-7c")
-		talker=ET.SubElement(stream, "talker")
-		StreamRank=ET.SubElement(talker, "stream-rank" )
-		Rank=ET.SubElement(StreamRank, "rank")
-		Rank.text=("0")
-		endStainter=ET.SubElement(talker, "end-station-interfaces")# ("end"+"-"+"station"+"-"+"interfaces"))
-		Mac=ET.SubElement(endStainter, "mac-address")
-		Mac.text=("00-0a-35-00-01-10")
-		intName=ET.SubElement(endStainter, "interface-name")
-		DF=ET.SubElement(talker, "data-frame-specification")
-		Index= ET.SubElement(DF, "index")
-		Index.text=("1")
-		IpTup= ET.SubElement(DF, "ipv4-tuple")
-		SrcIP= ET.SubElement (IpTup, "source-ip-address")
-		SrcIP.text=(ListOfIpAddress[i])#("10.1.1.11")
-		DstIP= ET.SubElement (IpTup, "destination-ip-address")
-		DstIP.text=(ListOfIpAddress[i+1])#("10.1.1.12")
-		Dscp = ET.SubElement (IpTup, "dscp")
-		Dscp.text=("0")
-		Protocol = ET.SubElement (IpTup, "protocol")
-		Protocol.text=("17")
-		SrcPort =ET.SubElement (IpTup, "source-port")
-		SrcPort.text=("0")
-		DstPort =ET.SubElement (IpTup,"destination-port")
-		DstPort.text=("6012")
-		TrafSpec=ET.SubElement (talker, "traffic-specification")
-		Interval= ET.SubElement (TrafSpec, "interval")
-		Numerator= ET.SubElement (Interval, "numerator")
-		Numerator.text=("1")
-		Denominator=ET.SubElement (Interval, "denominator")
-		Denominator.text=("1000")
-		MFPintrval=ET.SubElement (TrafSpec, "max-frames-per-interval")
-		MFPintrval.text=ListOfFramesPerPeriod[i] #("1")
-		MFSiz=ET.SubElement (TrafSpec, "max-frame-size")
-		MFSiz.text = (ListOfMaxFrameSize[i]) #("1044")
-		TSelcttion=ET.SubElement(TrafSpec,"transmission-selection")
-		TSelcttion.text = ("0")
-		TimeAware=ET.SubElement(TrafSpec, "time-aware")
-		EarliestTranOffset=ET.SubElement(TimeAware, "earliest-transmit-offset")
-		EarliestTranOffset.text=("0")
-		LatestTransOffset=ET.SubElement(TimeAware, "latest-transmit-offset")
-		LatestTransOffset.text=("0")
-		Jitter=ET.SubElement(TimeAware, "jitter")
-		Jitter.text=("0")
-		UserToNtkReq=ET.SubElement(talker, "user-to-network-requirements")	
-		NumSemTrees=ET.SubElement(UserToNtkReq, "num-seamless-trees")
-		NumSemTrees.text=("1")
-		InterfacCap=ET.SubElement(talker, "interface-capabilities")
-		VlanTagCap=ET.SubElement(InterfacCap, "vlan-tag-capable")
-		VlanTagCap.text = ("true")
-		Listeners=ET.SubElement(stream, "listeners")
-		Index2= ET.SubElement(Listeners, "index")
-		Listener=ET.SubElement(Listeners,"listener")
-		EndStainter2=ET.SubElement(Listener, "end-station-interfaces")
-		Mac2=ET.SubElement(EndStainter2, "mac-address")
-		IntName=ET.SubElement(EndStainter2, "interface-name")
-		UserToNtkReq2=ET.SubElement(Listener, "user-to-network-requirements")
-		InterfacCap2=ET.SubElement(Listener, "interface-capabilities")
-		VlanTagCap=ET.SubElement(InterfacCap2, "vlan-tag-capable")
-
-	Qcctree = ET.ElementTree(Root_Qcc)
-	Qcctree.write(Qcc, pretty_print=True)
+    Root_Qcc= ET.Element("qcc-inputs")
+    Root_Qcc.set("xmlns", "urn:ieee:std:802.1Q:yang:ge-qcc")
+    for i in range (len(ListOfStreams)):
+        stream = ET.SubElement(Root_Qcc, "stream-configuration")
+        streamID=ET.SubElement(stream, "stream-id")
+        streamID.text=("00-0a-35-00-01-10:17-7"+str(i))#(ListOfStreams[i])#"00-0a-35-00-01-10:17-7c")
+        talker=ET.SubElement(stream, "talker")
+        StreamRank=ET.SubElement(talker, "stream-rank" )
+        Rank=ET.SubElement(StreamRank, "rank")
+        Rank.text=("0")
+        endStainter=ET.SubElement(talker, "end-station-interfaces")#("end"+"-"+"station"+"-"+"interfaces"))
+        Mac=ET.SubElement(endStainter, "mac-address")
+        Mac.text=(ListOfMacAddress[i%len(ListOfMacAddress)])#("00-0a-35-00-01-10")
+        #Mac2.text = (ListOfMacAddress[i])
+        intName=ET.SubElement(endStainter, "interface-name")
+        DF=ET.SubElement(talker, "data-frame-specification")
+        Index= ET.SubElement(DF, "index")
+        Index.text=("1")
+        IpTup= ET.SubElement(DF, "ipv4-tuple")
+        SrcIP= ET.SubElement (IpTup, "source-ip-address")
+        SrcIP.text=(ListOfIpAddress[i%len(ListOfIpAddress)])#("10.1.1.11")
+        DstIP= ET.SubElement (IpTup, "destination-ip-address")
+        DstIP.text=(ListOfIpAddress[(i+1)%len(ListOfIpAddress)])#("10.1.1.12")
+        Dscp = ET.SubElement (IpTup, "dscp")
+        Dscp.text=("0")
+        Protocol = ET.SubElement (IpTup, "protocol")
+        Protocol.text=("17")
+        SrcPort =ET.SubElement (IpTup, "source-port")
+        SrcPort.text=("0")
+        DstPort =ET.SubElement (IpTup,"destination-port")
+        DstPort.text=("6012")
+        TrafSpec=ET.SubElement (talker, "traffic-specification")
+        Interval= ET.SubElement (TrafSpec, "interval")
+        Numerator= ET.SubElement (Interval, "numerator")
+        Numerator.text=("1")
+        Denominator=ET.SubElement (Interval, "denominator")
+        Denominator.text=("1000")
+        MFPintrval=ET.SubElement (TrafSpec, "max-frames-per-interval")
+        MFPintrval.text=("1")#ListOfFramesPerPeriod[i] 
+        MFSiz=ET.SubElement (TrafSpec, "max-frame-size")
+        MFSiz.text = ("1044")#(ListOfMaxFrameSize[i]) 
+        TSelcttion=ET.SubElement(TrafSpec,"transmission-selection")
+        TSelcttion.text = ("0")
+        TimeAware=ET.SubElement(TrafSpec, "time-aware")
+        EarliestTranOffset=ET.SubElement(TimeAware, "earliest-transmit-offset")
+        EarliestTranOffset.text=("0")
+        LatestTransOffset=ET.SubElement(TimeAware, "latest-transmit-offset")
+        LatestTransOffset.text=("0")
+        Jitter=ET.SubElement(TimeAware, "jitter")
+        Jitter.text=("0")
+        UserToNtkReq=ET.SubElement(talker, "user-to-network-requirements")	
+        NumSemTrees=ET.SubElement(UserToNtkReq, "num-seamless-trees")
+        NumSemTrees.text=("1")
+        InterfacCap=ET.SubElement(talker, "interface-capabilities")
+        VlanTagCap=ET.SubElement(InterfacCap, "vlan-tag-capable")
+        VlanTagCap.text= ("true")
+        Listeners=ET.SubElement(stream, "listeners")
+        Index2= ET.SubElement(Listeners, "index")
+        Index2.text=("0")
+        Listener=ET.SubElement(Listeners,"listener")
+        EndStainter2=ET.SubElement(Listener, "end-station-interfaces")
+        Mac2=ET.SubElement(EndStainter2, "mac-address")
+        Mac2.text = (ListOfMacAddress[(i+1)%len(ListOfMacAddress)])
+        IntName=ET.SubElement(EndStainter2, "interface-name")
+        UserToNtkReq2=ET.SubElement(Listener, "user-to-network-requirements")
+        InterfacCap2=ET.SubElement(Listener, "interface-capabilities")
+        VlanTagCap1=ET.SubElement(InterfacCap2, "vlan-tag-capable")
+        VlanTagCap1.text= ("true")
+    Qcctree = ET.ElementTree(Root_Qcc)
+    Qcctree.write(Qcc, pretty_print=True)
 	
 ################## Main Function ####################
 if __name__ == "__main__":
